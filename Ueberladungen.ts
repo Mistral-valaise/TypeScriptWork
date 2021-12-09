b620e381a4d7fe40e9cdc0a9336aa27f48513071
mistral <nvalaise@outlook.de>
2021-12-09 07:58:23 +0100

    20211208-Fix

--- Ueberladungen.ts
+++ Ueberladungen.ts
@@ -1,23 +1,25 @@
-function add (value1: string, value2: string)  //{} das hier erstmal weg lassen 
-function add(value1: number, value2: number)//{} auch erstmal weg lassen 
-function add (value1: any, value2: any){
-    if(typof value1 === "string"){
-        value1 = parseFloat(value1);
-    }
-    value2 = +value2;
-    var result = value1 + value2;
-    console.log(result);
-    
-} 
+function add(value1: string, value2: string); //{} das hier erstmal weg lassen
+function add(value1: number, value2: number); //{} auch erstmal weg lassen
+function add(value1: any, value2: any) {
+  if (typeof value1 === "string") {
+    value1 = parseFloat(value1);
+  }
+  value2 = +value2;
+  var result = value1 + value2;
+  console.log(result);
+}
 
-function multiply (value1: number, value2: number){
-    var result = value1 * value2;
-    console.log(result);
+// zeite variante
+function multiply(value1: number | string, value2: number | string) {
+  value1 = +value1;
+  value2 = +value2;
+  var result = value1 * value2;
+  console.log(result);
 }
 
 add(3, 5);
 add("3", "5");
-add(3, true);
-// NO GO --> add(2, "5");
+
+// NO GO --> add(2, "5");  und add(3, true);
 
 multiply(5, 10);
