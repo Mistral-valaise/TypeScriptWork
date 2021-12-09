19d5b9414df4f2c9ab3378e4103efcf3db252066
mistral <nvalaise@outlook.de>
2021-12-09 07:32:03 +0100

    Install TypeScript

---
+++ Ueberladungen.ts
@@ -0,0 +1,23 @@
+function add (value1: string, value2: string)  //{} das hier erstmal weg lassen 
+function add(value1: number, value2: number)//{} auch erstmal weg lassen 
+function add (value1: any, value2: any){
+    if(typof value1 === "string"){
+        value1 = parseFloat(value1);
+    }
+    
+    var result = value1 + value2;
+    console.log(result);
+    
+} 
+
+function multiply (value1: number, value2: number){
+    var result = value1 * value2;
+    console.log(result);
+}
+
+add(3, 5);
+add("3", "5");
+add(3, true);
+// NO GO --> add(2, "5");
+
+multiply(5, 10);
