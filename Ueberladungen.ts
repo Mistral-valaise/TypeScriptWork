3445c096892c89d2171a38e37614dd4c0649fd7b
mistral <nvalaise@outlook.de>
2021-12-09 07:59:03 +0100

    Fix

--- Ueberladungen.ts
+++ Ueberladungen.ts
@@ -1,23 +1,22 @@
-function add (value1: string, value2: string)  //{} das hier erstmal weg lassen 
-function add(value1: number, value2: number)//{} auch erstmal weg lassen 
-function add (value1: any, value2: any){
-    if(typof value1 === "string"){
-        value1 = parseFloat(value1);
-    }
-    
-    var result = value1 + value2;
-    console.log(result);
-    
-} 
+function add(value1: string, value2: string); //{} das hier erstmal weg lassen
+function add(value1: number, value2: number); //{} auch erstmal weg lassen
+function add(value1: any, value2: any) {
+  if (typeof value1 === "string") {
+    value1 = parseFloat(value1);
+  }
 
-function multiply (value1: number, value2: number){
-    var result = value1 * value2;
-    console.log(result);
+  var result = value1 + value2;
+  console.log(result);
+}
+
+function multiply(value1: number, value2: number) {
+  var result = value1 * value2;
+  console.log(result);
 }
 
 add(3, 5);
 add("3", "5");
-add(3, true);
+//add(3, true);
 // NO GO --> add(2, "5");
 
 multiply(5, 10);
